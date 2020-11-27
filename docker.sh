2d2649235d147ec87981cedcfae9bfcab4d5a424
taras <taras@prom.lan>
2020-11-26 21:23:46 -0800

    added alert manager

--- docker.sh
+++ docker.sh
@@ -1,24 +1,23 @@
-docker rm -f power-exporter 
 docker rm -f node-ping
 docker rm -f prometheus
-docker rm -f grafana
-docker rm -f kindle-planet
+docker rm -f dhcp-leases2file_sd
+docker rm -f alertmanager
 set -x -e
-docker run --restart on-failure -d -p 8000:8080 --name kindle-planet tarasglek/kindle-planet  
-docker run --restart on-failure -d -p 3000:3000 --name node-ping tarasglek/node-ping  
-docker run --restart on-failure -d -p 8080:8080 --name power-exporter -e RAINFOREST=192.168.1.215 tarasglek/power-exporter:2
+docker run --restart on-failure -d -p 3000:3000 --name node-ping node-ping  
+docker run --restart on-failure -d -v /export/firewalls:/input -v /tmp:/output  --name dhcp-leases2file_sd  dhcp-leases2file_sd
+docker run --restart on-failure -d -p 9093:9093 -v /home/taras/prom:/prometheus --name alertmanager prom/alertmanager --config.file=/prometheus/alertmanager.yml
 docker run --restart on-failure -d -p 81:9090 -v /home/taras/prom:/prometheus \
-  --name prometheus --link power-exporter \
-  --link node-ping \
-  prom/prometheus -config.file=/prometheus/prometheus.yml -storage.local.retention 1460h0m0s
-docker run \
-  -d \
-  -p 80:3000 \
-  --name=grafana \
-  -e "GF_SERVER_ROOT_URL=http://swarm" \
-  -e "GF_SECURITY_ADMIN_PASSWORD=secret" \
-  -v /home/taras/prom/grafana/lib:/var/lib/grafana \
-  -v /home/taras/prom/grafana/log:/var/log/grafana \
-  -v /home/taras/prom/grafana/etc:/etc/grafana \
-  --link prometheus \
-  grafana/grafana
+  -v /tmp:/tmp \
+  --name prometheus --link node-ping  --link alertmanager \
+  prom/prometheus --config.file=/prometheus/prometheus.yml --storage.tsdb.retention.size=100GB --storage.tsdb.retention.time=365d
+# docker run \
+#   -d \
+#   -p 80:3000 \
+#   --name=grafana \
+#   -e "GF_SERVER_ROOT_URL=http://swarm" \
+#   -e "GF_SECURITY_ADMIN_PASSWORD=secret" \
+#   -v /home/taras/prom/grafana/lib:/var/lib/grafana \
+#   -v /home/taras/prom/grafana/log:/var/log/grafana \
+#   -v /home/taras/prom/grafana/etc:/etc/grafana \
+#   --link prometheus \
+#   grafana/grafana
